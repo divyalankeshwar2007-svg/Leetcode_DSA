62368f9e1431d322eb9fc91abcc05da575351fad
divyalankeshwar2007-svg <divyalankeshwar2007@gmail.com>
2026-04-01 19:43:46 +0530

    Time: 0 ms (100%), Space: 8.6 MB (64.96%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        int original = x;
+        long long rev = 0;
+        while(x!=0){
+            int digits = x % 10;
+            rev = rev * 10 + digits;
+            x = x / 10;
+        }
+        return original == rev;
+    }
+};
