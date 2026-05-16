112f71fa0e7873791a43087751fa4ac5cd37265c
divyalankeshwar2007-svg <divyalankeshwar2007@gmail.com>
2026-05-16 20:17:47 +0530

    Time: 0 ms (100%), Space: 15.6 MB (66.6%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
+
         int ind = -1;
+
         int n = nums.size();
+
         for(int i = n-2; i>=0; i--){
             if(nums[i] < nums[i+1]){
                 ind = i;
@@ -21,6 +24,7 @@ public:
                 break;
             }
         }
+        
         reverse(nums.begin() + ind + 1 , nums.end());
     }
 };
