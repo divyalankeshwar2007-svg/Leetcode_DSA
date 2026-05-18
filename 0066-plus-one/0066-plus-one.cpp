918534aad21e75377e25797e8da6fcdd2b96853d
divyalankeshwar2007-svg <divyalankeshwar2007@gmail.com>
2026-05-18 22:51:58 +0530

    Time: 0 ms (100%), Space: 11.4 MB (86.34%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
+
         int n = digits.size();
 
         for(int i = n-1 ; i >= 0 ; i--){
@@ -8,9 +9,13 @@ public:
                 digits[i]++;
                 return digits;
             }
+
             digits[i] = 0;
         }
+
         digits.insert(digits.begin(),1);
+
         return digits;
+        
     }
 };
