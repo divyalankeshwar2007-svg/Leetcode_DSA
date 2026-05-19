6e0c25c5eb82707adead220da2c74dd080927865
divyalankeshwar2007-svg <divyalankeshwar2007@gmail.com>
2026-05-19 22:58:58 +0530

    Time: 0 ms (100%), Space: 9.1 MB (46.36%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
+
         int ind = haystack.find(needle);
+        
         if(ind == string::npos){
             return -1;
         }
+
         return ind;
     }
 };
