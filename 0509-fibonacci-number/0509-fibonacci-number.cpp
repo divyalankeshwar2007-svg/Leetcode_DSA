c5080931c1338780c790e12adecbb35c604293b7
divyalankeshwar2007-svg <divyalankeshwar2007@gmail.com>
2026-06-01 23:20:32 +0530

    Time: 0 ms (100%), Space: 7.9 MB (38.52%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<=1){
+        int a = 0;
+        int b = 1;
+
+        if(n==0 || n==1){
             return n;
         }
-        int a = 0, b = 1;
-        int c;
+
         for(int i = 2; i<=n ; i++){
-            c = a + b;
+            int c = a + b;
             a = b;
             b = c;
         }
