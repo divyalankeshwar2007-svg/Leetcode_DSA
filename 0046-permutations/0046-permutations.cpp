7e7ae898388eb196e86aea069d781ebe941c2ec9
divyalankeshwar2007-svg <divyalankeshwar2007@gmail.com>
2026-06-16 09:47:03 +0530

    Time: 0 ms (100%), Space: 10.6 MB (70.21%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    void recurpermute(int ind,vector<int>& arr,vector<vector<int>>& ans){
+        if(ind==arr.size()){
+            ans.push_back(arr);
+            return;
+        }
+        for(int i = ind; i<arr.size(); i++){
+            swap(arr[ind],arr[i]);
+            recurpermute(ind+1,arr,ans);
+            swap(arr[i],arr[ind]);
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int ind;
+        recurpermute(0,nums,ans);
+        return ans;
+    }
+};
