fd3a15c9e49c574ea911e4dfcba2ddb81dd96b76
divyalankeshwar2007-svg <divyalankeshwar2007@gmail.com>
2026-06-27 14:07:32 +0530

    Time: 62 ms (28.74%), Space: 61.8 MB (42.88%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> path;
+        func(0,s,ans,path);
+        return ans;
+    }
+
+    void func(int ind,string s,vector<vector<string>>& ans,vector<string>& path){
+        if(ind == s.size()){
+            ans.push_back(path);
+            return;
+        }
+
+        for(int i = ind; i<s.size();++i){
+            if(isPallindrome(s,ind,i)){
+                path.push_back(s.substr(ind,i-ind+1));
+                func(i+1,s,ans,path);
+                path.pop_back();
+            }
+        }
+    }
+
+    bool isPallindrome(string s, int start,int end){
+        while(start<=end){
+            if(s[start++] != s[end--]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
