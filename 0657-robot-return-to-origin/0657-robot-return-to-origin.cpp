c01faf562691a79c308432c424f4cbff1bfdd5e6
divyalankeshwar2007-svg <divyalankeshwar2007@gmail.com>
2026-07-20 19:29:08 +0530

    Time: 4 ms (26.28%), Space: 10.2 MB (76.99%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int n = moves.size();
+        int a = 0;
+        int b = 0;
+
+        for(int i = 0;i<n; i++){
+
+            if(moves[i] == 'U'){
+                a++;
+            }
+            else if(moves[i] == 'D'){
+                a--;
+            }
+            else if(moves[i] == 'L'){
+                b++;
+            }
+            else{
+                b--;
+            }
+        }
+
+        if(a == 0 && b==0){
+            return true;
+        }
+        else return false;
+    }
+};
